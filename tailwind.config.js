66ee2b943230d464a1a10a2dd4bcf46cd2efade8
Benjamin Hansen <benjamin.hansen44@gmail.com>
2020-04-17 14:03:32 +0200

    Tailwin config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Primary Colors
+        sage: 'var(--sage)',
+        rust: 'var(--rust)',
+        drab: 'var(--drab)',
+        danger: 'var(--danger)',
+
+        // Secondary Colors
+        ice:'var(--ice)',
+        fossil:'var(--fossil)',
+        sand:'var(--sand)',
+
+        // Typography Colors
+        onyx: 'var(--onyx)',
+        slate:'var(--slate)',
+        snow:'var(--snow)',
+      },
+      fontFamily:{
+        primary:["Roboto", "sans-serif"],
+        alt:["Kosugi Maru", "sans-serif"]
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
